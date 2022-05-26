99bf7ace48ea7e02fbf5bb55d6222da28292d226
Hopes-Chantel <Hopes.Chantel@gmail.com>
2022-05-26 16:50:48 -0400

    deploy

--- routes/api/like.js
+++ routes/api/like.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { Like } = require("../../models/Like");
-const { Dislike } = require("../../models/dislike");
+// const { Like } = require("../../models/Like");
+// const { Dislike } = require("../../models/dislike");
 
 
 router.post("/getLikes", (req, res) => {
